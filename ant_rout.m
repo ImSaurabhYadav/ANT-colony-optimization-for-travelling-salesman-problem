3bfabfb21cef060bb1ead5656689706940d2b9fe
Saurabh Yadav <saurabhyadav@hotmail.com>
2018-04-09 03:21:21 +0530

    Solution

---
+++ ant_rout.m
@@ -0,0 +1,21 @@
+function [new_places]=ant_tour(start_places,m,n,h,t,alpha,beta);
+for i=1:m
+    mh=h;
+    for j=1:n-1
+        c=start_places(i,j);
+        mh(:,c)=0;
+        temp=(t(c,:).^beta).*(mh(c,:).^alpha);
+        s=(sum(temp));
+        p=(1/s).*temp;
+        r=rand;
+        s=0;
+        for k=1:n
+            s=s+p(k);
+            if r<=s
+                start_places(i,j+1)=k;
+                break
+            end
+        end
+    end
+end
+new_places=start_places;
